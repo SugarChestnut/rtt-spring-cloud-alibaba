8c2382312cc7eeb1ec98cab5d9e8291d91ec0fa0
rtt <1191623980@qq.com>
2024-06-16 22:44:51 +0800

    0616

---
+++ rtt-product/src/main/java/cn/rentaotao/product/RttProductApplication.java
@@ -0,0 +1,13 @@
+package cn.rentaotao.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RttProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RttProductApplication.class, args);
+    }
+
+}
---
+++ rtt-product/src/test/java/cn/rentaotao/product/RttProductApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.rentaotao.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RttProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
